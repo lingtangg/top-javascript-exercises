8802a7b82c53b800426b1adc9b50dcf8b143d41f
lingtangg <60630600+lingtangg@users.noreply.github.com>
2022-04-28 15:45:27 +1000

    Halfway there

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    phrase = Array.from(word);
+    reversed = '';
+    for (let i = phrase.length - 1; i >= 0; i--) {
+        reversed += phrase[i];
+    }
+    return reversed;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...moreArgs) {
+    for (let i = 0; i < moreArgs.length; i++) {
+        index = array.indexOf(moreArgs[i]);
+        if (index == -1) {
+            continue
+        }
+        array.splice(index, 1);
+    }
+    return array;
 };
 
 // Do not edit below this line
